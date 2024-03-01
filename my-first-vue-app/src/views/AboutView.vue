be86674f1d53db1002cc168d4717187804d69fff
Dmytro Somliev <dmytro.somliev@ibm.com>
2024-03-01 09:37:34 +0100

    - generate a vue project - Mixins and Composable

---
+++ my-first-vue-app/src/views/AboutView.vue
@@ -0,0 +1,13 @@
+<script setup>
+    import {useFlash} from "@/composables/useFlash.js";
+
+    let { flash } = useFlash();
+</script>
+
+<template>
+    <div class="about">
+        <p>
+            <button @click="flash('It works')">Click Me</button>
+        </p>
+    </div>
+</template>
---
+++ my-first-vue-app/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+    import {useFlash} from "@/composables/useFlash.js";
+
+    let { flash } = useFlash();
+</script>
+
+<template>
+    <main>
+        <button @click="flash('It works')">Click Me</button>
+    </main>
+</template>
