852d7dbad6bcd3b6b7213ce64f8b45cd73d86782
Dmytro Somliev <dmytro.somliev@ibm.com>
2024-03-06 08:29:16 +0100

    - creating an inputs with dummy tests

--- source/src/views/sign-up/SignUp.vue
+++ source/src/views/sign-up/SignUp.vue
@@ -1,3 +1,27 @@
 <template>
     <h1>Sign Up</h1>
+
+    <section>
+        <label for="username">Username</label>
+        <input id="username">
+    </section>
+
+    <section>
+        <label for="username">Mail</label>
+        <input id="email">
+    </section>
+
+    <section>
+        <label for="password">Password</label>
+        <input id="password" type="password">
+    </section>
+
+    <section>
+        <label for="passwordRepeat">Password Repeat</label>
+        <input id="passwordRepeat" type="password">
+    </section>
+
+    <button disabled>Sign Up</button>
 </template>
+<script setup>
+</script>
--- source/src/views/sign-up/__tests__/SignUp.test.js
+++ source/src/views/sign-up/__tests__/SignUp.test.js
@@ -1,13 +1,63 @@
 import { render, screen } from '@testing-library/vue';
 import SignUp from '@/views/sign-up/SignUp.vue';
-import { expect } from 'vitest';
+import { expect, describe, it, beforeEach } from 'vitest';
 
 describe('Sign Up', () => {
-    it('should have Sign Up header', () => {
+    beforeEach(() => {
         render(SignUp);
-        
+    });
+    
+    it('should have Sign Up header', () => {
         const header = screen.getByRole('heading', {name: 'Sign Up'});
         
         expect(header).toBeInTheDocument();
+    });
+    
+    it('should have username input', () => {
+        const username = screen.queryByLabelText('Username', {});
+        
+        expect(username).toBeInTheDocument();
+    });
+    
+    it('should have email input', () => {
+        const email = screen.queryByLabelText('Mail', {});
+        
+        expect(email).toBeInTheDocument();
+    });
+    
+    it('should have password input', () => {
+        const password = screen.queryByLabelText('Password', {});
+        
+        expect(password).toBeInTheDocument();
+    });
+    
+    it('should have type of password input for password input', () => {
+        const password = screen.queryByLabelText('Password', {});
+        
+        expect(password).toHaveAttribute('type', 'password');
+    });
+    
+    it('should have password repeat input', () => {
+        const password = screen.queryByLabelText('Password Repeat', {});
+        
+        expect(password).toBeInTheDocument();
+    });
+    
+    it('should have type of password input for password repeat input', () => {
+        const passwordRepeat = screen.queryByLabelText('Password Repeat', {});
+        
+        expect(passwordRepeat).toHaveAttribute('type', 'password');
+    });
+    
+    it('should have Sign Up button', () => {
+        const button = screen.getByRole('button', {name: 'Sign Up'});
+        
+        expect(button).toBeInTheDocument();
+    });
+    
+    it('should have Sign Up Button disabled initially', () => {
+        const button = screen.getByRole('button', {name: 'Sign Up'});
+        
+        expect(button).toBeDisabled();
     });
 });
