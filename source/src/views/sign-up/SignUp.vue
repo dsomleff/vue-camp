f708d01617fbb5d3cf60b178b5beb771f3ba4620
Dmytro Somliev <dmytro.somliev@ibm.com>
2024-04-08 16:19:44 +0200

    - add form submit prevent

--- source/src/views/sign-up/SignUp.vue
+++ source/src/views/sign-up/SignUp.vue
@@ -16,6 +16,7 @@ const isDisabled = computed(() => {
 });
 
 const submit = () => {
+    // eslint-disable-next-line no-unused-vars
     const {passwordRepeat, ...body} = formState;
     axios.post('/api/v1/users', body);
 }
@@ -24,44 +25,45 @@ const submit = () => {
 <template>
     <h1>Sign Up</h1>
 
-    <p>
-        <label for="username">Username </label>
-        <input
-            id="username"
-            v-model="formState.username"
-        >
-    </p>
+    <form @submit.prevent="submit">
+        <p>
+            <label for="username">Username </label>
+            <input
+                id="username"
+                v-model="formState.username"
+            >
+        </p>
 
-    <p>
-        <label for="email">E-Mail </label>
-        <input
-            id="email"
-            v-model="formState.email"
-        >
-    </p>
+        <p>
+            <label for="email">E-Mail </label>
+            <input
+                id="email"
+                v-model="formState.email"
+            >
+        </p>
 
-    <p>
-        <label for="password">Password </label>
-        <input
-            id="password"
-            v-model="formState.password"
-            type="password"
-        >
-    </p>
+        <p>
+            <label for="password">Password </label>
+            <input
+                id="password"
+                v-model="formState.password"
+                type="password"
+            >
+        </p>
 
-    <p>
-        <label for="passwordRepeat">Password Repeat </label>
-        <input
-            id="passwordRepeat"
-            v-model="formState.passwordRepeat"
-            type="password"
-        >
-    </p>
+        <p>
+            <label for="passwordRepeat">Password Repeat </label>
+            <input
+                id="passwordRepeat"
+                v-model="formState.passwordRepeat"
+                type="password"
+            >
+        </p>
 
-    <button
-        :disabled="isDisabled"
-        @click="submit"
-    >
-        SignUp
-    </button>
+        <button
+            :disabled="isDisabled"
+        >
+            SignUp
+        </button>
+    </form>
 </template>
